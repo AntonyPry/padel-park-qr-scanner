a068b97c8de878d36aac3718f010f91249c0aa08
antonypry <pritov_as@resanta.ru>
2026-04-25 22:11:52 +0300

    env

--- server/migrations/20260414212834-update-utilization-v2.js
+++ server/migrations/20260414212834-update-utilization-v2.js
@@ -3,16 +3,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // 1. Переименовываем старые колонки
-    await queryInterface.renameColumn('utilizations', 'booked6', 'booked1');
-    await queryInterface.renameColumn('utilizations', 'booked15', 'booked2');
+    await queryInterface.renameColumn('Utilizations', 'booked6', 'booked1');
+    await queryInterface.renameColumn('Utilizations', 'booked15', 'booked2');
 
     // 2. Добавляем новые колонки для сессий
-    await queryInterface.addColumn('utilizations', 'sessions1', {
+    await queryInterface.addColumn('Utilizations', 'sessions1', {
       type: Sequelize.INTEGER,
       defaultValue: 0,
       allowNull: false,
     });
-    await queryInterface.addColumn('utilizations', 'sessions2', {
+    await queryInterface.addColumn('Utilizations', 'sessions2', {
       type: Sequelize.INTEGER,
       defaultValue: 0,
       allowNull: false,
@@ -21,9 +21,9 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     // Откат изменений
-    await queryInterface.removeColumn('utilizations', 'sessions1');
-    await queryInterface.removeColumn('utilizations', 'sessions2');
-    await queryInterface.renameColumn('utilizations', 'booked1', 'booked6');
-    await queryInterface.renameColumn('utilizations', 'booked2', 'booked15');
+    await queryInterface.removeColumn('Utilizations', 'sessions1');
+    await queryInterface.removeColumn('Utilizations', 'sessions2');
+    await queryInterface.renameColumn('Utilizations', 'booked1', 'booked6');
+    await queryInterface.renameColumn('Utilizations', 'booked2', 'booked15');
   },
 };
